3de7b54775d31299c889cb442e696d035abed332
Robert Filic <robert.filic@gmail.com>
2025-08-01 23:47:17 +0200

    Adding private leaderboards

--- app/private-leagues/[id]/leaderboard/page.tsx
+++ app/private-leagues/[id]/leaderboard/page.tsx
@@ -1,17 +1,3 @@
-//import LeagueLeaderboardClient from './LeagueLeaderboardClient';
-
-// This interface correctly defines the shape of the props for an async page
-//interface Props {
- // params: Promise<{ leagueId: string }>;
-}
-
-// This is now an async Server Component that correctly awaits the params
-//export default async function LeagueLeaderboardPage({ params }: Props) {
- // const { leagueId } = await params;
-
- // return <LeagueLeaderboardClient leagueId={leagueId} />;
-}
-
 // app/private-leagues/[id]/leaderboard/page.tsx
 import PrivateLeagueLeaderboardClientPage from './PrivateLeagueLeaderboardClientPage';
 
